29ebf32b1075d8be0693ebf946c6dbc5064e1d61
Alfux <alexis.t.fuchs@gmail.com>
2025-11-27 20:17:18 +0100

    Part 1: pie and bar charts v1.0

--- src/distribution.py
+++ src/distribution.py
@@ -1,12 +1,18 @@
 """Distribution program. Detect a folder architecture and plot charts."""
 import argparse as arg
 from argparse import Namespace
-import json
 import logging
 import os
 from pathlib import Path
 import sys
 from typing import Self, Generator
+
+from matplotlib.axes import Axes
+from matplotlib.container import BarContainer
+from matplotlib.figure import Figure
+from matplotlib.patches import Wedge
+from matplotlib.text import Text
+import matplotlib.pyplot as plt
 
 
 class Distribution:
@@ -21,6 +27,14 @@ class Distribution:
         self._path = path if isinstance(path, Path) else Path(path)
         self._arch = dict(self._architecture(self._path))
         self._ctgy = dict(self._categories(self._arch))
+        self._fg: Figure = plt.figure(figsize=(16, 9))
+        self._pie: Axes = self._fg.add_axes([0.05, 0.15, 0.4, 0.75])
+        self._bar: Axes = self._fg.add_axes([0.55, 0.15, 0.4, 0.75])
+        self._set_pie_bar(self._pie, self._bar)
+
+    def show(self: Self) -> None:
+        """Show the plot."""
+        plt.show()
 
     def _architecture(self: Self, path: Path) -> Generator:
         """Architecture generator.
@@ -55,6 +69,27 @@ class Distribution:
                 yield (key, sub)
         yield ("count", count)
 
+    def _set_pie_bar(
+            self: Self, pie: Axes, bar: Axes
+    ) -> tuple[tuple[list[Wedge], list[Text], list[Text]], BarContainer]:
+        """Set the pie chart.
+
+        Args:
+            pie (Axes): The Axes to draw the pie chart on.
+            bar (Axes): The Axes to draw the bar chart on.
+        Returns:
+            tuple: (list of wedges, list of labels).
+        """
+        counts = [c["count"] for k, c in self._ctgy.items() if k[0] == '_']
+        labels = [c[1:] for c in self._ctgy.keys() if c[0] == '_']
+        wdg, lbl, pct = pie.pie(counts, labels=labels, autopct="%.1f%%")
+        tks = list(range(len(counts)))
+        ctn = bar.bar(tks, counts, color=[w.get_facecolor() for w in wdg])
+        bar.set_xticks(tks)
+        bar.set_xticklabels(labels)
+        plt.setp(bar.get_xticklabels(), rotation=-45)
+        return ((wdg, lbl, pct), ctn)
+
 
 def get_args(description: str = '') -> Namespace:
     """Manages program arguments.
@@ -83,7 +118,7 @@ def main() -> int:
             logging.basicConfig(level=logging.DEBUG, format=fmt)
         else:
             logging.basicConfig(level=logging.INFO, format=fmt)
-        Distribution(av.path).explore()
+        Distribution(av.path).show()
     except Exception as err:
         debug = "av" in locals() and hasattr(av, "debug") and av.debug
         logging.critical("Fatal error: %s", err, exc_info=debug)
